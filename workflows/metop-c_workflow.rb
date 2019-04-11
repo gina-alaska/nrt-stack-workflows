bbb6518bd3a98d261e64b5ddf155f5886a889a32
Jay C <jay@alaska.edu>
2019-04-11 20:56:11 +0000

    Metop C updated.
    Signed-off-by: Jay C <jay@alaska.edu>

--- workflows/metop-c_workflow.rb
+++ workflows/metop-c_workflow.rb
@@ -16,6 +16,9 @@ steps[:l1] = Step.where(name: "MetopC_L1").first_or_create({
   command: "metop_l1.rb -s M03 -t {{workspace}} {{job.input_path}} {{job.output_path}}",
   parent: steps[:l0]
 })
+
+#----------------------------------                   AVHRR
+# AWIPS
 steps[:awips] = Step.where(name: "MetopC_Awips").first_or_create({
   processing_level: ProcessingLevel.where(name: 'awips').first_or_create,
   queue: 'polar2grid',
@@ -23,6 +26,8 @@ steps[:awips] = Step.where(name: "MetopC_Awips").first_or_create({
   command: "metop_awips.rb -t {{workspace}} {{job.input_path}} {{job.output_path}}",
   parent: steps[:l1]
 })
+
+#AWIPS LDM
 steps[:ldm] = Step.where(name: "MetopC_LDMInject").first_or_create({
   command: "pqinsert.rb {{job.input_path}}",
   queue: 'ldm',
@@ -39,6 +44,8 @@ steps[:scmi_awips] = Step.where(name: "MetopC_SCMIAwips").first_or_create({
   command: "awips_scmi.rb -m avhrr -t {{workspace}} {{job.input_path}} {{job.output_path}}",
   parent: steps[:l1]
 })
+
+#SCMI LDM
 steps[:scmi_ldm] = Step.where(name: "MetopC_SCMILDMInject").first_or_create({
   command: "pqinsert.rb {{job.input_path}}",
   queue: 'ldm',
@@ -47,23 +54,57 @@ steps[:scmi_ldm] = Step.where(name: "MetopC_SCMILDMInject").first_or_create({
   enabled: false
 })
 
-
-steps[:mirs_level2] = Step.where(name: "MetopC_MirsL2").first_or_create({
+#----------------------------------                   MIRS
+# L2
+steps[:mirs_level2] = Step.where(name: "MetopCMirsL2").first_or_create({
   processing_level: ProcessingLevel.where(name: 'l1').first_or_create,
   queue: 'cspp_extras',
   sensor: Sensor.where(name: 'avhrr').first_or_create,
   command: "mirs_l0.rb -s metop-c -t {{workspace}} {{job.input_path}} {{job.output_path}}",
+  enabled: false,
   parent: steps[:l1]
 })
 
-steps[:mirs_awips] = Step.where(name: "MetopC_MirsAwips").first_or_create({
+#AWIPS 
+steps[:mirs_awips] = Step.where(name: "MetopCMirsAwips").first_or_create({
   processing_level: ProcessingLevel.where(name: 'l1').first_or_create,
   queue: 'polar2grid',
   sensor: Sensor.where(name: 'avhrr').first_or_create,
   command: "mirs_awips.rb -s amsu -t {{workspace}} {{job.input_path}} {{job.output_path}}",
+  parent: steps[:mirs_level2],
+  enabled: false
+})
+
+
+#AWIPS LDM
+steps[:mirs_awips_ldm] = Step.where(name: "MetopSCMILDMInject").first_or_create({
+  command: "pqinsert.rb {{job.input_path}}",
+  queue: 'ldm',
+  producer: false,
+  parent: steps[:mirs_awips],
+  enabled: false
+})
+
+#SCMI 
+steps[:mirs_scmi] = Step.where(name: "MetopCMirsSCMI").first_or_create({
+  processing_level: ProcessingLevel.where(name: 'mirs_scmi').first_or_create,
+  queue: 'polar2grid',
+  enabled: false,
+  sensor: Sensor.where(name: 'avhrr').first_or_create,
+  command: "awips_scmi.rb -m mirs -t {{workspace}} {{job.input_path}} {{job.output_path}}",
   parent: steps[:mirs_level2]
 })
 
+#SCMI LDM
+steps[:mirs_scmi_ldm] = Step.where(name: "MetopCSCMILDMInject").first_or_create({
+  command: "pqinsert.rb {{job.input_path}}",
+  queue: 'ldm',
+  producer: false,
+  parent: steps[:mirs_scmi],
+  enabled: false
+})
+
+#---------------------------------                    GEOTIFF
 
 steps[:geotiff_l1] = Step.where(name: "MetopC_GeoTiff_l1").first_or_create(
   processing_level: ProcessingLevel.where(name: 'geotiff_l1').first_or_create,
@@ -78,6 +119,9 @@ steps[:geotiff_l2] = Step.where(name: "MetopC_GeoTiff_l2").first_or_create(
   command: "feeder_geotif.rb -m metop-c -t {{workspace}} {{job.input_path}} {{job.output_path}}",
   parent: steps[:geotiff_l1])
 
+
+#---------------------------------   DONE.
+
 steps[:geotiff_l1].requirements = %w[polar2grid].map do |requirement|
   Requirement.where(name: requirement).first_or_create
 end
@@ -85,11 +129,6 @@ end
 steps[:geotiff_l2].requirements = %w[geotiff].map do |requirement|
   Requirement.where(name: requirement).first_or_create
 end
-
-
-
-
-
 
 # Set up requirements
 steps[:l0].requirements = %w{aapp}.map do |requirement|
