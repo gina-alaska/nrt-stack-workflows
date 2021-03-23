ccdfb4693276547f4b78e98240a5e9775fd9e506
Jay C <jay@alaska.edu>
2021-03-23 21:20:21 +0000

    fire geotifs.

---
+++ workflows/fire_geotifs.rb
@@ -0,0 +1,24 @@
+steps = {}
+#VIIRS SDR
+steps[:viirs_sdr] = Step.where(name: "Noaa20ViirsSdrJob").first
+
+#VIIRS GEOTIFS
+steps[:viirs_gm_geotiff] = Step.where(name: "Noaa20ViirsGeoTiffGm").first_or_create({
+  command: "p2g_geotif.rb -m viirs_gm -t {{workspace}} {{job.input_path}} {{job.output_path}}",
+  queue: 'polar2grid',
+  processing_level: ProcessingLevel.where(name: 'geotiff_gm_l1').first_or_create,
+  sensor: Sensor.where(name: 'viirs').first_or_create,
+  parent: steps[:viirs_sdr]
+})
+
+#VIIRS FEEDER Geotifs
+steps[:viirs_gm_feeder] = Step.where(name: "Noaa20ViirsGeoTiffGm2").first_or_create({
+  command: "feeder_geotif.rb -m noaa20gm -t {{workspace}} {{job.input_path}} {{job.output_path}}",
+  queue: 'geotiff',
+  processing_level: ProcessingLevel.where(name: 'geotiff_gm_l2').first_or_create,
+  sensor: Sensor.where(name: 'viirs').first_or_create,
+  parent: steps[:viirs_geotiff]
+})
+
+satellite = Satellite.friendly.find('noaa20')
+satellite.workflows << steps[:arrival] unless satellite.workflows.include?(steps[:arrival])
