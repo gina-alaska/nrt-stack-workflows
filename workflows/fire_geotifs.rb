c8132aa5365db607e40a75445d86364a303c4139
JC <spruceboy@gmail.com>
2021-03-23 15:49:20 -0800

    Update fire_geotifs.rb
    Had sdr name wrong..

--- workflows/fire_geotifs.rb
+++ workflows/fire_geotifs.rb
@@ -23,11 +23,11 @@ steps[:viirs_gm_feeder] = Step.where(name: "Noaa20ViirsGeoTiffGm2").first_or_cre
 steps = {}
 #*******                 SNPP
 #VIIRS SDR
-steps[:viirs_sdr] = Step.where(name: "SnppViirsSdrJob").first
+steps[:viirs_sdr] = Step.where(name: "ViirsSdrJob").first
 
 #
 #VIIRS GEOTIFS
-steps[:viirs_gm_geotiff] = Step.where(name: "SnppViirsGeoTiffGm").first_or_create({
+steps[:viirs_gm_geotiff] = Step.where(name: "NppViirsGeoTiffGm").first_or_create({
   command: "p2g_geotif.rb -m viirs_gm -t {{workspace}} {{job.input_path}} {{job.output_path}}",
   queue: 'polar2grid',
   processing_level: ProcessingLevel.where(name: 'geotiff_gm_l1').first_or_create,
@@ -36,8 +36,8 @@ steps[:viirs_gm_geotiff] = Step.where(name: "SnppViirsGeoTiffGm").first_or_creat
 })
 
 #VIIRS FEEDER Geotifs
-steps[:viirs_gm_feeder] = Step.where(name: "SnppViirsGeoTiffGm2").first_or_create({
-  command: "feeder_geotif.rb -m noaa20gm -t {{workspace}} {{job.input_path}} {{job.output_path}}",
+steps[:viirs_gm_feeder] = Step.where(name: "NppViirsGeoTiffGm2").first_or_create({
+  command: "feeder_geotif.rb -m snpp20gm -t {{workspace}} {{job.input_path}} {{job.output_path}}",
   queue: 'geotiff',
   processing_level: ProcessingLevel.where(name: 'geotiff_gm_l2').first_or_create,
   sensor: Sensor.where(name: 'viirs').first_or_create,
