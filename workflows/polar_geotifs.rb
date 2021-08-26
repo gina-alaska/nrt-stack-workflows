d256b8af0219af2747b1116ad878e21a5060779c
JC <spruceboy@gmail.com>
2021-08-25 16:43:41 -0800

    Update polar_geotifs.rb

--- workflows/polar_geotifs.rb
+++ workflows/polar_geotifs.rb
@@ -20,13 +20,12 @@ steps[:noaa20_viirs_polar_feeder] = Step.where(name: "Noaa20ViirsGeoTiffPolarFee
   parent: steps[:noaa20_viirs_polar_geotiff]
 })
 
-steps = {}
+
 
 #*******                 SNPP
 #VIIRS SDR
+steps = {}
 steps[:viirs_sdr] = Step.where(name: "ViirsSdrJob").first
-
-#
 #VIIRS GEOTIFS
 steps[:snpp_viirs_polar_geotiff] = Step.where(name: "NppViirsGeoTiffPolar").first_or_create({
   command: "p2g_geotif.rb -m viirs_polar -t {{workspace}} {{job.input_path}} {{job.output_path}}",
@@ -46,9 +45,8 @@ steps[:snpp_viirs_polar_feeder] = Step.where(name: "NppViirsGeoTiffPolarFeeder")
 })
 
 # MODIS
+steps = {}
 steps[:terra_l1] = Step.where(name: "TerraSeadas").first
-steps[:aqua_l1] = Step.where(name: "AquaSeadas").first
-
 steps[:terra_polar_geotiff] = Step.where(name: "TerraModisGeoTiffPolar").first_or_create({
   command: "p2g_geotif.rb -m modis_polar  -p 4 -t {{workspace}} {{job.input_path}} {{job.output_path}}",
   queue: 'polar2grid',
@@ -56,15 +54,6 @@ steps[:terra_polar_geotiff] = Step.where(name: "TerraModisGeoTiffPolar").first_o
   sensor: Sensor.where(name: 'modis').first_or_create,
   parent: steps[:terra_l1]
    })
-
-steps[:aqua_polar_geotiff] = Step.where(name: "AquaModisGeoTiffPolar").first_or_create({
-  command: "p2g_geotif.rb -m modis_polar  -p 4 -t {{workspace}} {{job.input_path}} {{job.output_path}}",
-  queue: 'polar2grid',
-  processing_level: ProcessingLevel.where(name: 'geotiff_polar_l1').first_or_create,
-  sensor: Sensor.where(name: 'modis').first_or_create,
-  parent: steps[:aqua_l1]
-   })
-
 steps[:terra_modis_polar_feeder] = Step.where(name: "TerraGeoTiffPolarFeeder").first_or_create({
   command: "feeder_geotif.rb -m terra_polar -t {{workspace}} {{job.input_path}} {{job.output_path}}",
   queue: 'geotiff',
@@ -73,6 +62,15 @@ steps[:terra_modis_polar_feeder] = Step.where(name: "TerraGeoTiffPolarFeeder").f
   parent: steps[:terra_polar_geotiff]
 })
 
+steps = {}
+steps[:aqua_l1] = Step.where(name: "AquaSeadas").first
+steps[:aqua_polar_geotiff] = Step.where(name: "AquaModisGeoTiffPolar").first_or_create({
+  command: "p2g_geotif.rb -m modis_polar  -p 4 -t {{workspace}} {{job.input_path}} {{job.output_path}}",
+  queue: 'polar2grid',
+  processing_level: ProcessingLevel.where(name: 'geotiff_polar_l1').first_or_create,
+  sensor: Sensor.where(name: 'modis').first_or_create,
+  parent: steps[:aqua_l1]
+   })
 steps[:aqua_modis_polar_feeder] = Step.where(name: "AquaGeoTiffPolarFeeder").first_or_create({
   command: "feeder_geotif.rb -m aqua_polar -t {{workspace}} {{job.input_path}} {{job.output_path}}",
   queue: 'geotiff',
@@ -80,4 +78,3 @@ steps[:aqua_modis_polar_feeder] = Step.where(name: "AquaGeoTiffPolarFeeder").fir
   sensor: Sensor.where(name: 'modis').first_or_create,
   parent: steps[:aqua_polar_geotiff]
 })
-
