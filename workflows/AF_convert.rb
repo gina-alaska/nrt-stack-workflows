43b5af9738818d70aaff1fa7f208607d251c41ad
Jay C <jay@alaska.edu>
2022-05-18 23:51:25 +0000

    Added SNPP.

--- workflows/AF_convert.rb
+++ workflows/AF_convert.rb
@@ -18,3 +18,21 @@ steps[:noaa20_fire_ldm] = Step.where(name: 'Noaa20ViirsFireConvertLDMJob').first
   enabled: true
 })
 
+steps[:snpp_fire] = Step.where(name: "SNPPViirsFireJob").first
+
+steps[:snpp_fire_convert] = Step.where(name: "SNPPViirsFireConvertJob").first_or_create({
+  command: "vaf_awips_reformat.rb t {{workspace}} {{job.input_path}} {{job.output_path}}",
+  queue: 'cspp_extras',
+  processing_level: ProcessingLevel.where(name: 'FireAwips').first_or_create,
+  sensor: Sensor.where(name: 'viirs').first_or_create,
+  parent: steps[:snpp_fire]
+})
+steps[:snpp_fire_ldm] = Step.where(name: 'SNPPViirsFireConvertLDMJob').first_or_create({
+  command: 'pqinsert.rb -t . {{job.input_path}}',
+  queue: 'ldm',
+  producer: false,
+  parent: steps[:snpp_fire_convert],
+  enabled: true
+})
+
+
