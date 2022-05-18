dc09b3d6322d4a5055181263c332c27606d072cc
Jay C <jay@alaska.edu>
2022-05-18 23:47:41 +0000

    Initial commit.

---
+++ workflows/AF_convert.rb
@@ -0,0 +1,20 @@
+steps = {}
+#*******                 NOAA20
+#VIIRS SDR
+steps[:noaa20_fire] = Step.where(name: "Noaa20ViirsFireJob").first
+
+steps[:noaa20_fire_convert] = Step.where(name: "Noaa20ViirsFireConvertJob").first_or_create({
+  command: "vaf_awips_reformat.rb t {{workspace}} {{job.input_path}} {{job.output_path}}",
+  queue: 'cspp_extras',
+  processing_level: ProcessingLevel.where(name: 'FireAwips').first_or_create,
+  sensor: Sensor.where(name: 'viirs').first_or_create,
+  parent: steps[:noaa20_fire]
+})
+steps[:noaa20_fire_ldm] = Step.where(name: 'Noaa20ViirsFireConvertLDMJob').first_or_create({
+  command: 'pqinsert.rb -t . {{job.input_path}}',
+  queue: 'ldm',
+  producer: false,
+  parent: steps[:noaa20_fire_convert],
+  enabled: true
+})
+
