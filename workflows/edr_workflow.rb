e12b537152619e001583074ee997a6176df3f3cc
JC <jay@alaska.edu>
2025-04-23 15:41:08 -0800

    Update edr_workflow.rb

--- workflows/edr_workflow.rb
+++ workflows/edr_workflow.rb
@@ -16,17 +16,19 @@ sources.each do |task|
                                                                          command: 'viirs_edr.rb -p 8 -t {{workspace}} {{job.input_path}} {{job.output_path}}',
                                                                          queue: 'edr',
                                                                          producer: true,
-                                                                         level: "level2",
+                                                                         processing_level: ProcessingLevel.where(name: 'level2').first_or_create,
+                                                                         sensor: Sensor.where(name: 'viirs').first_or_create,
                                                                          enabled: true,
-                                                                         parent: steps[parrent]
+                                                                         parent: steps[parent]
                                                                        })
   scmi_task_sym = (task_stub + 'EdrScmiJob').to_sym
   steps[scmi_task_sym] = Step.where(name: task_stub + 'ScmiEdrJob').first_or_create({
                                                                          command: 'awips_scmi.rb -m edr  -t {{workspace}} {{job.input_path}} {{job.output_path}}',
                                                                          queue: 'polar2grid',
                                                                          producer: true,
-                                                                         level: "edr_scmi",
                                                                          enabled: true,
+                                                                         processing_level: ProcessingLevel.where(name: 'edr_scmi').first_or_create,
+                                                                         sensor: Sensor.where(name: 'viirs').first_or_create,
                                                                          parent: steps[task_sym]
                                                                        })
   geotiff_task_sym = (task_stub + 'EdrGeotiffJob').to_sym
@@ -34,7 +36,8 @@ sources.each do |task|
                                                                          command: 'p2g_geotif.rb -m edr  -t {{workspace}} {{job.input_path}} {{job.output_path}}',
                                                                          queue: 'polar2grid',
                                                                          producer: true,
-                                                                         level: "edr_geotiff_l1",
+                                                                         processing_level: ProcessingLevel.where(name: 'edr_geotiff_l1').first_or_create,
+                                                                         sensor: Sensor.where(name: 'viirs').first_or_create,
                                                                          enabled: true,
                                                                          parent: steps[task_sym]
                                                                        })
